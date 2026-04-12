5bcbee611226c4c934d3b4492cf4fb592d5a91f7
Diego Tenjo <tenjodiego961@gmail.com>
2026-04-12 14:08:38 -0500

    fix(backend): add custom providers in auth.config to accept JWTs issued by vercel deployment

--- packages/backend/convex/auth.config.ts
+++ packages/backend/convex/auth.config.ts
@@ -2,5 +2,19 @@ import { getAuthConfigProvider } from "@convex-dev/better-auth/auth-config";
 import type { AuthConfig } from "convex/server";
 
 export default {
-  providers: [getAuthConfigProvider()],
+  providers: [
+    getAuthConfigProvider(),
+    {
+      domain: "https://barberstudio-bm.vercel.app",
+      applicationID: "convex",
+    },
+    {
+      domain: "https://barberstudio.jdevs.me",
+      applicationID: "convex",
+    },
+    {
+      domain: "http://localhost:3001",
+      applicationID: "convex",
+    }
+  ],
 } satisfies AuthConfig;
